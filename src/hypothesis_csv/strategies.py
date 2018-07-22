f523a4145a9d8ef9e798adcd1e13a011e6278652
Simone Robutti <simone.robutti@teraki.com>
2018-07-22 22:16:05 +0200

    added csv call

--- src/hypothesis_csv/strategies.py
+++ src/hypothesis_csv/strategies.py
@@ -1,6 +1,10 @@
 from hypothesis.strategies import composite, integers, floats, text, one_of, sampled_from
-from hypothesis import settings,seed
+from hypothesis import settings, seed
 from hypothesis.errors import InvalidArgument
+from meza.convert import records2csv
+import string
+import functools
+
 
 def is_seq(x):
     is_seq = True
@@ -12,8 +16,10 @@ def is_seq(x):
     return is_seq
 
 
-def get_columns(draw,columns_param):
-    valid_column_types = [integers, floats, text]
+def get_columns(draw, columns_param):
+    valid_column_types = [integers, floats,
+                          functools.partial(text, min_size=1, max_size=10,
+                                            alphabet=string.ascii_lowercase + string.ascii_uppercase + string.digits)]
 
     if is_seq(columns_param):
         columns = columns_param
@@ -29,19 +35,36 @@ def get_columns(draw,columns_param):
 
     return columns
 
-def get_lines_num(draw,lines_param):
+
+def get_lines_num(draw, lines_param):
     if lines_param is None:
         return draw(integers(min_value=1, max_value=100))
-    elif isinstance(lines_param,int):
+    elif isinstance(lines_param, int):
         return lines_param
     else:
         raise InvalidArgument("lines param must be an integer")
 
+
 @composite
 def data_rows(draw, lines=None, columns=None):
-
-    columns = get_columns(draw,columns)
-    lines_num = get_lines_num(draw,lines)
+    columns = get_columns(draw, columns)
+    lines_num = get_lines_num(draw, lines)
     rows = [tuple(draw(column) for column in columns) for _ in range(lines_num)]
 
     return rows
+
+
+@composite
+def csv(draw, header=None, *args, **kwargs):
+    if not header:
+        final_header_len = draw(integers(min_value=1, max_value=10))
+        header = []
+        while (len(header) < final_header_len):
+            new_column = draw(
+                text(min_size=1, alphabet=string.ascii_lowercase + string.ascii_uppercase + string.digits))
+            if new_column not in header:
+                header.append(new_column)
+
+    rows = draw(data_rows(columns=len(header), *args, **kwargs))
+    data = [dict(zip(header, d)) for d in rows]
+    return records2csv(data).getvalue()
--- tests/test_strategies.py
+++ tests/test_strategies.py
@@ -1,14 +1,22 @@
-from hypothesis_csv.strategies import data_rows
+from hypothesis_csv.strategies import data_rows, csv
 from hypothesis import strategies as st
-from hypothesis import settings, given
+from hypothesis import settings, given, reproduce_failure
+from meza.io import read_csv
+from io import StringIO
+import string
+
+
+def csv2records(string):
+    return list(read_csv(StringIO(string)))
 
 
 @settings(use_coverage=False)
-@given(data=st.data(),lines_num=st.integers(min_value=0, max_value=200))
-def test_data_rows_fixed_lines_num(data,lines_num):
+@given(data=st.data(), lines_num=st.integers(min_value=0, max_value=200))
+def test_data_rows_fixed_lines_num(data, lines_num):
     rows = data.draw(data_rows(lines=lines_num, columns=[st.integers(), st.integers(), st.floats()]))
     assert len(rows) == lines_num
     assert all([len(r) == 3 for r in rows])
+
 
 @settings(use_coverage=False)
 @given(data=st.data())
@@ -17,17 +25,58 @@ def test_data_rows_random_lines_num(data):
     assert len(rows) >= 1
     assert all([len(r) == 3 for r in rows])
 
+
 @settings(use_coverage=False)
 @given(data=st.data())
 def test_data_rows_all_random(data):
     rows = data.draw(data_rows())
     assert len(rows) >= 1
-    assert all([len(r) >=1 for r in rows])
+    assert all([len(r) >= 1 for r in rows])
 
 
 @settings(use_coverage=False)
-@given(data=st.data(),columns_num=st.integers(min_value=1, max_value=10))
-def test_data_rows_fixed_column_num(data,columns_num):
+@given(data=st.data(), columns_num=st.integers(min_value=1, max_value=10))
+def test_data_rows_fixed_column_num(data, columns_num):
     rows = data.draw(data_rows(columns=columns_num))
     assert len(rows) >= 1
-    assert all([len(r) == columns_num and all([type(field) in [int,str,float]] for field in r) for r in rows])
+    assert all([len(r) == columns_num and all([type(field) in [int, str, float]] for field in r) for r in rows])
+
+
+@settings(use_coverage=False)
+@given(data=st.data())
+def test_csv_all_random(data):
+    csv_string = data.draw(csv())
+
+    assert isinstance(csv_string, str)
+
+
+@settings(use_coverage=False)
+@given(data=st.data())
+def test_csv_header_simple(data):
+    header = ["abc", "cde"]
+    csv_string = data.draw(csv(header=header, lines=5))
+    records = csv2records(csv_string)
+
+    extracted_header = list(records[0].keys())
+    assert extracted_header == header
+
+
+@settings(use_coverage=False)
+@given(data=st.data(), header=st.lists(st.text(min_size=1, max_size=5,
+                                               alphabet=string.ascii_lowercase + string.ascii_uppercase + string.digits),
+                                       min_size=1, max_size=5))
+def test_csv_header_property(data, header):
+    header = list(set(header))
+    csv_string = data.draw(csv(header=header))
+    records = csv2records(csv_string)
+    extracted_header = list(records[0].keys())
+    assert extracted_header == header
+
+
+@settings(use_coverage=False)
+@given(data=st.data())
+def test_csv_lines_fixed(data):
+    lines = 10
+    csv_string = data.draw(csv(lines=lines))
+    records = csv2records(csv_string)
+    assert len(records) == lines
